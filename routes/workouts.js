fd7803982fa6fc99124219d10244d1356b51e72f
ArtofCJC <artofcjc@gmail.com>
2023-08-08 19:25:34 -0400

    backend

---
+++ routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+
+//GET all workout
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
+
+//GET a single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET single workout'})
+})
+
+// POST new workoput
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'})
+})
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a new workout'})
+})
+
+//UPDATE a workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a new workout'})
+})
+
+module.exports = router
